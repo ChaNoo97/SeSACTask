621b02d2ce9a05ccfedb8c1fa716ffbc8d759109
kimchanhoo <cksgn0709@gmail.com>
2021-11-03 23:16:53 +0900

    sort 기능구현

--- SeSacWeek06Shoppinglist/SeSacWeek06Shoppinglist/MainViewController.swift
+++ SeSacWeek06Shoppinglist/SeSacWeek06Shoppinglist/MainViewController.swift
@@ -10,6 +10,9 @@ import RealmSwift
 
 class MainViewController: UIViewController {
 
+	
+	@IBOutlet weak var sortButton: UIButton!
+	
 	@IBOutlet weak var titleLabel: UILabel!
 	@IBOutlet weak var addTextFiled: UITextField!
 	@IBOutlet weak var addButton: UIButton!
@@ -28,6 +31,10 @@ class MainViewController: UIViewController {
 		
 		tasks = localRealm.objects(UserShoppinglist.self)
 		print(tasks)
+		
+		sortButton.setImage(UIImage(systemName: "list.bullet"), for: .normal)
+		sortButton.tintColor = .lightGray
+		
     }
 	
 	@objc func checkButtonClicked(sender: UIButton) {
@@ -45,7 +52,40 @@ class MainViewController: UIViewController {
 		}
 		tableView.reloadData()
 	}
+	
+	@IBAction func sortButtonClicked(_ sender: UIButton) {
+//		print("sort")
+//
+//		try! self.localRealm.write {
+//			tasks.sorted(byKeyPath: "checkBtn")
+//		}
+//		tableView.reloadData()
+		
+		
+		let alert = UIAlertController(title: "정렬", message: "정렬기준을 선택해주세요", preferredStyle: .actionSheet)
 
+		let checkStandard = UIAlertAction(title: "할일", style: .default) { action in
+			self.tasks = self.tasks.sorted(byKeyPath: "checkBtn", ascending: false)
+			self.tableView.reloadData()
+		}
+		let favoriteStandard = UIAlertAction(title: "즐겨찾기", style: .default) { action in
+			self.tasks = self.tasks.sorted(byKeyPath: "bookmarkBtn", ascending: false)
+			self.tableView.reloadData()
+		}
+
+		let titleStandard = UIAlertAction(title: "제목",  style: .default) { action in
+			self.tasks = self.tasks.sorted(byKeyPath: "content", ascending: true)
+			self.tableView.reloadData()
+		}
+		
+
+		alert.addAction(checkStandard)
+		alert.addAction(favoriteStandard)
+		alert.addAction(titleStandard)
+
+		present(alert, animated: true, completion: nil)
+	}
+	
 	@IBAction func addButtonClicked(_ sender: UIButton) {
 		if let text = addTextFiled.text {
 			let task = UserShoppinglist(content: text)
@@ -99,6 +139,20 @@ extension MainViewController: UITableViewDelegate, UITableViewDataSource {
 	
 	func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
 		return 60
+	}
+	
+//	func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+//		true
+//	}
+	
+	func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+		
+		let row = tasks[indexPath.row]
+		
+		try! localRealm.write {
+			localRealm.delete(row)
+			tableView.reloadData()
+		}
 	}
 	
 }
